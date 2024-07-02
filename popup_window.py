b86bd5b99d74d43946f62466c7a832dccaee69ae
Leon Dickens <leon.dickens@hotmail.com>
2024-07-02 22:00:26 +0100

    WIP: Adding in top level window support to the main window

---
+++ popup_window.py
@@ -0,0 +1,11 @@
+import customtkinter as ctk
+
+
+class PopupWindow(ctk.CTkToplevel):
+    def __init__(self, text_message: str = "", title: str = "", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.geometry("400x300")
+
+        self.text = text_message
+        self.label = ctk.CTkLabel(self, text=self.text)
+        self.label.pack()
